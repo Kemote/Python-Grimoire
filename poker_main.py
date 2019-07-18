e74d3654f5ec527216f395db4c1dc80246e4e7a3
Kemote <tomek.szymski@gmail.com>
2019-07-19 00:56:59 +0200

    it is necessary to check check_pair, check_two_pairs, because of logical error with deleting existing cards.
    Next step is to add 14 to returned value for final check which hand is better.

--- poker_main.py
+++ poker_main.py
@@ -72,7 +72,7 @@ class Deck: #klasa odpowiedzialna za tworzenie talii
    def deal(self, num_cards):  #odpowiada za rozdanie kart graczom, zamysl jest taki zeby podawac ilosc kart w locie
        if self.__cards_in_game + num_cards > 52:
            print("Koniec talii")
-           return True #pozawala wyjść z funkcji gdy brakuje kart
+           return False #pozawala wyjść z funkcji gdy brakuje kart
        _hand = []
        for i in range(num_cards):
            self.__cards[self.__cards_in_game].state_hand_set = True #zaznacza w tali, ze karta w grze
@@ -85,23 +85,193 @@ class Deck: #klasa odpowiedzialna za tworzenie talii
 
 #testowy modul rozgrywki dla dwych graczy----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 class Stool:
-   def __init__(self, deck, *args):
+   def __init__(self, *args):
        self.players = (args) #przekonwertuje tuple na liste, arg podaje argumenty w psotaci tupli warotsci tupli nie da sie edytowac
-       self.deck = deck
+       #self.deck = deck
        self.pool = 0
-       self.cards_on_stool = ()
+       self.cards_on_stool = []
 
-   def check_poker_hand(self):
-       def check_streight:
+   def deal(self, deck, x):
+       self.cards_on_stool += deck.deal(x)
+
+   def cards(self): #jako,ze ta funkcja się powtarza mozna sprobowac jako dekorator
+       cards_on_hands = []
+       for i in range(len(self.cards_on_stool)):
+           cards_on_hands.append([self.cards_on_stool[i].color, self.cards_on_stool[i].figure])
+       return cards_on_hands
+
+   def check_poker_hand(self, player):
+
+       cards = self.players[player].cards() + self.cards
+       print(cards)
+       for i in cards:
+           if i[1] == 'A':
+               i[1] = '1'
+           elif i[1] == 'J':
+               i[1] = '11'
+           elif i[1] == 'Q':
+               i[1] = '12'
+           elif i[1] == 'K':
+               i[1] = '13'
+
+       def check_high(cards):
+           high = [int(i[1]) for i in cards]
+           if min(high) == 1:
+               return True, 14, "High card"
+           return True, max(high), "High card"
+
+       def check_one_pair(cards):
+           pair = [int(i[1]) for i in cards]
+           for i in range(len(pair)):
+               if pair[i] == 1:
+                   pair[i] = 14
+           pair.sort()
+           pair.reverse()
+           for i in pair:
+               if pair.count(i) == 2:
+                   return True, i, "Pair"
+           return False, 0, "None"
+
+       # na bank poprawic szyszukiwania dwuch par!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+       def check_two_pairs(cards):
+           first_pair = check_one_pair(cards)
+           if first_pair[0]:
+               for i in cards:
+                   if i[1] == first_pair[1]:
+                       cards.remove(i)
+               if first_pair[1] == '14':
+                   for i in cards:
+                       if i[1] == '1':
+                           cards.remove(i)
+               cards.reverse()
+               sec_pair = check_one_pair(cards)
+               if sec_pair[0]:
+                   return first_pair, sec_pair, "Two pair"
+           return False, 0, "None"
+
+       def check_three_kind(cards):
+           kind = [str(i[1]) for i in cards]
+           kind.sort()
+           for i in kind:
+               if kind.count(i) == 3:
+                   if int(i) == 1: i = 14
+                   return True, i, "Three of kind"
+           return False, 0, "None"
+
+       def check_streight(cards):
+           streight = [int(i[1]) for i in cards]
+
+           for i in streight:
+               if streight.count(i) != 1:
+                   streight.remove(i)
+           streight.sort()
+           print(streight)
+           if len(streight) < 5:
+               return False, 0, "None"
+           for i in range(len(streight) - 4):
+               if streight[-i] - streight[-i - 4] == 4:
+                   return True, streight[-i], "Streight"
+           if streight[-1] - streight[-4] == 3 and streight[0] == 1:
+               return True, 14, "Streight"
+
+           return False, 0
+
+       def check_flush(cards):
+           color = [i[0] for i in cards]
+           for i in ['spades', 'hearts', 'clubs', 'diamonds']:
+               if color.count(i) >= 5:
+                   return True, i, "Flush"
+           return False, 0, "None"
+
+       def check_full_house(cards):
+           three = check_three_kind(cards)
+           if three[0]:
+               for i in cards:
+                   if i[1] == three[1]:
+                       cards.remove(i)
+               if three[1] == '14':
+                   for i in cards:
+                       if i[1] == '1':
+                           cards.remove(i)
+               three_2 = check_three_kind(cards)
+               if three_2[0]:
+                   return True, three, three_2, "Full house"
+               pair = check_one_pair(cards)
+               if pair[0]:
+                   return True, three, pair, "Full house"
+           return False, 0, "None"
+
+       def check_four_of_kind(cards):
+           kind = [str(i[1]) for i in cards]
+           kind.sort()
+           for i in kind:
+               if kind.count(i) == 4:
+                   if int(i) == 1: i = 14
+                   return True, i, "Four of kind"
+           return False, 0, "None"
+
+       def check_poker(cards):
+           def check():
+               for i in range(1, 3):
+                   if (int(cards[-i][1]) - int(cards[-i - 4][1]) == 4 and check_flush(cards[-i - 4:-i])):
+                       return True, cards[-i][1], "Streight flush"
+               return False, 0, "None"
+
+           cards = sorted(cards, key=lambda x: int(x[1]))
+           result = check()
+           if result[0]:
+               return result
+           for i in cards:
+               if i[1] == "1":
+                   i[1] = "14"
+           cards = sorted(cards, key=lambda x: int(x[1]))
+           result = check()
+           return result
+
+       if check_poker(cards)[0]:
+           return check_poker(cards)
+       if check_four_of_kind(cards)[0]:
+           return check_four_of_kind(cards)
+       if check_full_house(cards)[0]:
+           return check_full_house(cards)
+       if check_flush(cards)[0]:
+           return check_flush(cards)
+       if check_streight(cards)[0]:
+           return check_streight(cards)
+       if check_three_kind(cards)[0]:
+           return check_three_kind(cards)
+       if check_two_pairs(cards)[0]:
+           return check_two_pairs(cards)
+       if check_one_pair(cards)[0]:
+           return check_one_pair(cards)
+       if check_high(cards)[0]:
+           return check_high(cards)
 
 
 
+#testowa rozgrywka--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 print("===|POKEROWA SUPER GRA|===|A||K||Q||J|===" + "\n")
-name = input("Player name:")
-player_one = Player(name, 1000)
-print("Hi " + name + " you have 1000 chips to play with my bot")
+#name = input("Player name:")    making player is last thing to do
+#player_one = Player(name, 1000)
 
+player0 = Player("Kemote",1000)
+player1 = Player("Alehandro", 1000)
+talia0 = Deck()
+
+player0.deal(talia0)
+player1.deal(talia0)
+
+print("Hi " + player0.name + " and " + player1.name +" you booth have 1000 chips to play \n")
+
+game1 = Stool(player0, player1)
+print(player0.name + " cards " + str(player0.cards))
+print(player1.name + " cards " + str(player1.cards) + "\n")
+
+game1.deal(talia0, 5)
+
+print("Karta na stole " + str(game1.cards()) + "\n")
+print(game1.check_poker_hand(0))
 
 
 #konies testowego modulu rozgrywki dla dwych graczy----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
